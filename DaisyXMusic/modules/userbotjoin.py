e70659f947304294547a9d7c25774105208ed22a
snxoyaze <82806256+snxoyaze@users.noreply.github.com>
2022-03-01 21:16:19 +0400

    Create userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -124,7 +124,7 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @Darkwebazerbaijan to your Group and try again</b>",
         )
         return
     await message.reply_text(
